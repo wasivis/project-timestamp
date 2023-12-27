2d50997f16588779da2866fea9ebf1ffa5834eca
Wasivis <ivanmontequin@gmail.com>
2023-12-26 21:07:53 -0300

    First commit

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ var app = express();
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
 // so that your API is remotely testable by FCC 
 var cors = require('cors');
-app.use(cors({optionsSuccessStatus: 200}));  // some legacy browsers choke on 204
+app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
 // http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
@@ -18,13 +18,32 @@ app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
+// Timestamp Microservice endpoint
+app.get("/api/:date", function (req, res) {
+  // Get the date parameter from the URL
+  const { date } = req.params;
 
-// your first API endpoint... 
-app.get("/api/hello", function (req, res) {
-  res.json({greeting: 'hello API'});
+  // Create a Date object based on the provided parameter
+  let inputDate;
+  if (/^\d+$/.test(date)) {
+    // If it's a Unix timestamp
+    inputDate = new Date(parseInt(date));
+  } else {
+    // If it's a date string
+    inputDate = new Date(date);
+  }
+
+  // Check if the input is a valid date
+  if (isNaN(inputDate.getTime())) {
+    return res.json({ error: "Invalid Date" });
+  }
+
+  // Respond with the Unix timestamp and UTC time
+  res.json({
+    unix: inputDate.getTime(),
+    utc: inputDate.toUTCString(),
+  });
 });
-
-
 
 // listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
