934cecdfefa2d0aa42e6a6d802ea134355f422c1
Wasivis <ivanmontequin@gmail.com>
2023-12-27 14:23:37 -0300

    Delete some comments and serve "views" folder.

--- index.js
+++ index.js
@@ -1,7 +1,3 @@
-// index.js
-// where your node app starts
-
-// init project
 var express = require('express');
 var app = express();
 
@@ -10,42 +6,33 @@ var app = express();
 var cors = require('cors');
 app.use(cors({ optionsSuccessStatus: 200 }));  // some legacy browsers choke on 204
 
-// http://expressjs.com/en/starter/static-files.html
 app.use(express.static('public'));
+app.use(express.static('views'));
 
-// http://expressjs.com/en/starter/basic-routing.html
 app.get("/", function (req, res) {
   res.sendFile(__dirname + '/views/index.html');
 });
 
-// Timestamp Microservice endpoint
 app.get("/api/:date", function (req, res) {
-  // Get the date parameter from the URL
   const { date } = req.params;
 
-  // Create a Date object based on the provided parameter
   let inputDate;
   if (/^\d+$/.test(date)) {
-    // If it's a Unix timestamp
     inputDate = new Date(parseInt(date));
   } else {
-    // If it's a date string
     inputDate = new Date(date);
   }
 
-  // Check if the input is a valid date
   if (isNaN(inputDate.getTime())) {
     return res.json({ error: "Invalid Date" });
   }
 
-  // Respond with the Unix timestamp and UTC time
   res.json({
     unix: inputDate.getTime(),
     utc: inputDate.toUTCString(),
   });
 });
 
-// listen for requests :)
 var listener = app.listen(process.env.PORT, function () {
   console.log('Your app is listening on port ' + listener.address().port);
 });
