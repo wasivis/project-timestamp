26f2c0c70afee4a5e87399f0978f948c930c2f1f
Ivan <87397558+wasivis@users.noreply.github.com>
2023-12-28 01:41:58 -0300

    Update index.js

--- index.js
+++ index.js
@@ -9,24 +9,25 @@ app.use(cors({ optionsSuccessStatus: 200 })); // some legacy browsers choke on 2
 app.use(express.static("public"));
 app.use(express.static("views"));
 
-app.get("/", function (req, res) {
-  res.sendFile(__dirname + "/views/index.html");
-});
-
-app.get("/api/:date", function (req, res) {
-  const { date } = req.params;
-
+app.get("/api/:date?", function (req, res) {
   let inputDate;
-  if (/^\d+$/.test(date)) {
-    inputDate = new Date(parseInt(date));
+
+  if (req.params.date) {
+    const { date } = req.params;
+
+    if (/^\d+$/.test(date)) {
+      inputDate = new Date(parseInt(date));
+    } else {
+      inputDate = new Date(date);
+    }
+
+    if (isNaN(inputDate.getTime())) {
+      return res.json({ error: "Invalid Date" });
+    }
   } else {
-    inputDate = new Date(date);
+    inputDate = new Date();
   }
-
-  if (isNaN(inputDate.getTime())) {
-    return res.json({ error: "Invalid Date" });
-  }
-
+  
   res.json({
     unix: inputDate.getTime(),
     utc: inputDate.toUTCString(),
